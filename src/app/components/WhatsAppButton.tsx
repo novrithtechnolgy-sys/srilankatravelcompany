f5ab3459aad1bec05a4e9666528db6d76263e7f6
GSNSAMPATH <novrithtechnolgy@gmail.com>
2026-05-30 13:44:43 +0530

    SLTC WEB UPDATE 5.30 4

--- src/app/components/WhatsAppButton.tsx
+++ src/app/components/WhatsAppButton.tsx
@@ -3,7 +3,7 @@
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/+94768132040"
+      href="https://wa.me/+9477234567"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50"
